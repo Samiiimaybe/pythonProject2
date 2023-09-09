233f4bd57916312229383ca5d2a7f943e4e3c1ef
Samiiimaybe <135401998+Samiiimaybe@users.noreply.github.com>
2023-09-10 01:53:56 +0530

    Initial Commit

--- main.py
+++ main.py
@@ -3,10 +3,39 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings
 import yfinance as yf
-
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
 
 print("My script starting")
-ticker=yf.Ticker('SBUX').info
-print(ticker)
+#It gives latest stock information for any particular stock
+#ticker=yf.Ticker('SBUX').info
+
+#It prints the data that we got from Yfinance for the stock
+#print(ticker)
+
+#we are entering start date and end date for the time period which we want the data for
+start_date = '2021-01-01'
+end_date = '2022-12-8'
+ticker = 'AAPL'
+data = yf.download(ticker,start_date,end_date)
+print("we are printing 1st 5 rows of stocks")
+
+#prints the 1st 5 rows of the data
+print(data.head())
+
+print("we are printing the last 5 rows of stocks")
+#prints the last 5 rows of the data
+print(data.tail())
 print("My script completed")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+#xaxis = np.array([8,12,16])
+# yaxis = np.array([6,9,12])
+
+#  plt.plot(xaxis,yaxis)
+# plt.show()
+
+
+data['Adj Close'].plot()
+plt.show()
--- stocks project.py
+++ stocks project.py
@@ -6,7 +6,16 @@ import yfinance as yf
 
 
 print("My script starting")
-ticker=yf.Ticker('SBUX').info
-print(ticker)
-print("My script completed")
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#It gives latest stock information for any particular stock
+#ticker=yf.Ticker('SBUX').info
+
+#It prints the data that we got from Yfinance for the stock
+#print(ticker)
+
+#we are entering start date and end date for the time period which we want the data for
+start_date = '2021-01-01'
+end_date = '2023-08-25'
+ticker = 'AAPL'
+data = yf.download(ticker,start_date,end_date)
+print("we are printing 1st 5 rows of stocks")
+
